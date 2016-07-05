f7d8dc0b68ce2b1b97a85f6d33ffac214e12a7c0
egordoga <edoga@ukr.net>
2016-07-05 07:27:35 +0300

    evrything

--- src/parking/Area.java
+++ src/parking/Area.java
@@ -5,9 +5,35 @@ package parking;
  */
 public class Area {
 
-    int place;
+    int numberPlace;
     int typeId;
-    int quantityDays;
-    Date startDate;
-    Date endDate;
+    boolean freePlace = true;
+    //int quantityDays;
+    //Date startDate;
+    //Date endDate;
+
+    Area place[] = new Area[50];
+
+    private void fillId{
+        for (int i = 0; i < 10; i++) {
+            typeId = 1;
+            //place.
+        }
+        for (int i = 10; i < 40; i++) {
+            typeId = 2;
+        }
+        for (int i = 40; i < 45; i++) {
+            typeId = 3;
+        }
+        for (int i = 45; i < 50; i++) {
+            typeId = 4;
+        }
+    }
+
+    private boolean isFree{
+
+    }
+
+    private void parkCar
+
 }
--- src/parking/Client.java
+++ src/parking/Client.java
@@ -5,43 +5,15 @@ package parking;
  */
 public class Client {
 
-    private String name;
-    private String phone;
-    private Car car;
+     String name;
+     String phone;
+     Car car;
 
-    public Client(String name, String phone, Car car) {
-        this.name = name;
-        this.phone = phone;
-        this.car = car;
+     Client[] list = new Client[10];
+     int size = 0;
 
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Car getCar() {
-        return car;
-    }
-
-    public void setCar(Car car) {
-        this.car = car;
-    }
-
-    Client client = new Client(getName(), getPhone(), getCar());
-
-    public Client addClient;
-
+     public void addClient(Client client) {
+        list[size] = client;
+        size++;
+     }
 }
