975d7e4eb33f275e1ba369dcfe4bba9827b2ec7c
egordoga <edoga@ukr.net>
2016-07-07 08:35:21 +0300

    evrything11

---
+++ src/parking/TestPlace.java
@@ -0,0 +1,13 @@
+package parking;
+
+/**
+ * Created by Master on 06.07.2016.
+ */
+public class TestPlace {
+    public static void main(String[] args) {
+        Area area = new Area();
+        //area.setArea();
+        area.setPlace();
+        area.showFree();
+    }
+}
