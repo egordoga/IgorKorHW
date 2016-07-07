225bc618eacd0753bd608b673ed6744690476cd8
egordoga <edoga@ukr.net>
2016-07-07 08:29:32 +0300

    evrything11

--- src/parking/Area.java
+++ src/parking/Area.java
@@ -7,33 +7,61 @@ public class Area {
 
     int numberPlace;
     int typeId;
-    boolean freePlace = true;
+    boolean freePlace;
     //int quantityDays;
     //Date startDate;
     //Date endDate;
 
+
     Area place[] = new Area[50];
 
-    private void fillId{
-        for (int i = 0; i < 10; i++) {
-            typeId = 1;
-            //place.
+    /*public  void setArea() {
+        this.numberPlace = numberPlace;
+        this.typeId = typeId;
+        this.freePlace = freePlace;
+    }*/
+
+    public   void setPlace(){
+
+        for (int i = 0; i <10; i++) {
+            // Area()
+            place[i] = new Area();
+            place[i].typeId = 1;
+            place[i].numberPlace = i + 1;
+            place[i].freePlace = true;
         }
         for (int i = 10; i < 40; i++) {
-            typeId = 2;
+            place[i] = new Area();
+            place[i].typeId = 2;
+            place[i].numberPlace = i + 1;
+            place[i].freePlace = false;
         }
         for (int i = 40; i < 45; i++) {
-            typeId = 3;
+            place[i] = new Area();
+            place[i].typeId = 3;
+            place[i].numberPlace = i + 1;
+            place[i].freePlace = true;
         }
         for (int i = 45; i < 50; i++) {
-            typeId = 4;
+            place[i] = new Area();
+            place[i].typeId = 4;
+            place[i].numberPlace = i + 1;
+            place[i].freePlace = false;
         }
-    }
 
-    private boolean isFree{
 
     }
 
-    private void parkCar
+    public void showFree(){
+        for (int i = 0; i < 50; i++) {
+           // place[i] = new Area();
+            if (place[i].freePlace){
+                System.out.print( place[i].numberPlace + ", " );
+            }
+        }
+
+    }
+
+    //private void parkCar*/
 
 }
